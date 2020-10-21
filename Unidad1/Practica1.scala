690d5ecd16a3e68be87649951c0bb23f9fabb1a0
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2020-10-20 22:05:51 -0700

    Update Practica1.scala

--- Unidad1/Practica1.scala
+++ Unidad1/Practica1.scala
@@ -1,11 +1,4 @@
 //1.Desarrollar un algoritmo en scala que calcule el radio de un círculo
-
-//3. Dada la variable bird = "tweet", utiliza interpolación de string para
-// imprimir "Estoy ecribiendo un tweet"
-//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la // secuencia "Luke"
-//5. ¿Cuál es la diferencia entre value y una variable en scala?
-//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el número 3.1416 
-import scala.math.sqrt
 val a =3
     val r = math.sqrt(a/math.Pi)
 //2. Desarrollar un algoritmo en scala que me diga si un número es primo
@@ -19,4 +12,9 @@ def numeroprimo(n :Int) : Boolean =
     else
     !(2 to (n-1)).exists(x=> n % x==0)
 }
+//3. Dada la variable bird = "tweet", utiliza interpolación de string para
+// imprimir "Estoy escribiendo un tweet"
 
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la // secuencia "Luke"
+//5. ¿Cuál es la diferencia entre value y una variable en scala?
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el número 3.1416 
